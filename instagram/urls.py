a3207fe7cda305faa1ba51e80a75a230a5632eae
MikeC <kiragumike@gmail.com>
2020-10-19 09:49:53 +0300

    rejistered urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,8 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
+
     # url('^$',views.index,name='index'),
     url(r'^imagedetails/(\d+)',views.imagedetails,name ='imagedetails'),
+    url(r'^new/image$',views.new_image,name='new-image'),
+    url(r'^new/comment$',views.new_comment,name='new-comment'),
+    
+    url(r'^create/profile$',views.create_profile,name='create-profile'),
+    url(r'^profile/',views.profile,name='profile'),
+    url(r'^profiledetails/(\d+)',views.profiledetails,name ='profiledetails'),
+    
+    url(r'^edit/profile$',views.edit_profile,name='edit-profile'),
+    url(r'^search/', views.search_results, name='search_results')
 
 ]
 if settings.DEBUG:
