3ab5dc3604d676d48b56606396783c7d3c6ed001
MikeC <kiragumike@gmail.com>
2020-10-19 10:32:31 +0300

    added tests

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Profile,Image,Comment
+import datetime as dt
+
+
+# Create your tests here.
+class ProfileTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.new_profile =Profile(profile_photo="'image.jpeg'",bio="God Above All")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+
+    # def test_update_bio(self):
+    #     self.new_profile.save_profile()
+    #     self.new_profile = Profile.objects.get(id=24)
+    #     profile = self.new_profile
+    #     profile.update_bio('changed user bio')
+    #     self.updated_profile = Profile.objects.get(id=24)
+    #     self.assertEqual(self.updated_profile.bio,'changed user bio')
+
+
+class ImageTestClass(TestCase):
+
+
+    def setUp(self):
+        self.new_image=Image(image="image.jpeg",name="Flower",caption="Naturelover",pub_date="two minutes ago",likes=4)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+
+    def test_save_method(self):
+        '''
+        Function that tests whether an image is saved to database
+        '''
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        '''
+        Function that tests whether an image can be deleted from the database
+        '''
+        self.new_image.save_image()
+        self.new_image.delete_image()
